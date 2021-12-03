9833d594ce02bf0fd82761289db586be88e75784
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-04 00:08:38 +0100

    Day 3 - part 2

--- day3.py
+++ day3.py
@@ -24,22 +24,37 @@ def get_wire_path(wire):
             yield x, y
 
 
-def get_cross_distance(wires):
+def get_crossing_min_distance(wires):
     crossings = set.intersection(*(set(get_wire_path(w)) for w in wires))
     distances = [abs(x) + abs(y) for x, y in crossings]
     return min(distances)
 
+def get_crossing_min_delay(wires):
+    paths = dict()
+    for w in wires:
+        first_visit = dict()
+        for step, pos in enumerate(get_wire_path(w)):
+            if pos not in first_visit:
+                first_visit[pos] = step
+        for pos, step in first_visit.items():
+            paths.setdefault(pos, []).append(step)
+    crossings = [steps for steps in paths.values() if len(steps) == 2]
+    steps = [sum(steps) + 2 for steps in crossings]
+    return min(steps)
 
 def run_tests():
     wires = ["R8,U5,L5,D3", "U7,R6,D4,L4"]
-    assert get_cross_distance(wires) == 6
+    assert get_crossing_min_distance(wires) == 6
+    assert get_crossing_min_delay(wires) == 30
     wires = ["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]
-    assert get_cross_distance(wires) == 159
+    assert get_crossing_min_distance(wires) == 159
+    assert get_crossing_min_delay(wires) == 610
 
 
 def get_solutions():
     wires = get_wires_from_file()
-    print(get_cross_distance(wires))
+    print(get_crossing_min_distance(wires))
+    print(get_crossing_min_delay(wires))
 
 
 if __name__ == "__main__":
