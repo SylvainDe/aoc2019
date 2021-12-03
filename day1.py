8edcc4539ea81676b593ff7491eaa4a122345219
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-03 19:23:33 +0100

    Day 1 - part 2

--- day1.py
+++ day1.py
@@ -11,18 +11,34 @@ def get_fuel_requirement(weight):
     return (weight // 3) - 2
 
 
+def get_fuel_requirement2(weight):
+    ret = 0
+    while True:
+         weight = (weight // 3) - 2
+         if weight <= 0:
+             break
+         ret += weight
+    return ret
+
+
 def get_full_fuel_requirement(modules):
     return sum(get_fuel_requirement(m) for m in modules)
+
+
+def get_full_fuel_requirement2(modules):
+    return sum(get_fuel_requirement2(m) for m in modules)
 
 
 def run_tests():
     modules = [12, 14, 1969, 100756]
     assert get_full_fuel_requirement(modules) == 2 + 2 + 654 + 33583
+    assert get_full_fuel_requirement2(modules) == 2 + 2 + 966 + 50346
 
 
 def get_solutions():
     modules = get_modules_from_file()
     print(get_full_fuel_requirement(modules))
+    print(get_full_fuel_requirement2(modules))
 
 
 if __name__ == "__main__":
