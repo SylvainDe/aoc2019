cb4fefb1cbb6b6a69d087da3bc99eb32b701fa6a
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-04 12:53:48 +0100

    Day 4 - part 2

--- day4.py
+++ day4.py
@@ -1,6 +1,6 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
-
+import collections
 
 def is_valid(n):
     s = str(n)
@@ -9,10 +9,17 @@ def is_valid(n):
         all(first <= second for first, second in zip(s, s[1:])))
 
 
-def get_passwords(valid_range):
+def is_valid2(n):
+    s = str(n)
+    return (len(s) == 6 and
+        2 in collections.Counter(s).values() and
+        all(first <= second for first, second in zip(s, s[1:])))
+
+
+def get_passwords(valid_range, func):
    mini, maxi = valid_range
    for n in range(mini, maxi):
-       if is_valid(n):
+       if func(n):
            yield n
 
 
@@ -20,11 +27,15 @@ def run_tests():
     assert is_valid(111111)
     assert not is_valid(223450)
     assert not is_valid(123789)
+    assert is_valid2(112233)
+    assert not is_valid2(123444)
+    assert is_valid2(111122)
 
 
 def get_solutions():
     input_range = 172930, 683082
-    print(len(list(get_passwords(input_range))))
+    print(len(list(get_passwords(input_range, is_valid))))
+    print(len(list(get_passwords(input_range, is_valid2))))
 
 
 if __name__ == "__main__":
