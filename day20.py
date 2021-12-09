29f22fab41aa96848affc814c4f49996ef59045c
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-09 19:26:27 +0100

    Day 20 - part 1

--- day20.py
+++ day20.py
@@ -2,10 +2,11 @@
 import datetime
 import string
 import collections
+import itertools
 
 def get_grid_from_file(file_path="day20_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        return [l for l in f]
 
 
 def points_iter(grid):
@@ -28,7 +29,7 @@ def solve_maze(grid):
             passages.add(pos)
         elif val in string.ascii_uppercase:
             letters[pos] = val
-        elif val not in " #":
+        elif val not in " #\n":
             assert False
     # Extract positions for labels
     labels = dict()
@@ -40,7 +41,6 @@ def solve_maze(grid):
             label = "".join(sorted(val + letter[0]))
             pos2 = passage[0]
             labels.setdefault(label, []).append(pos2)
-    print(labels)
     entrance = labels.pop("AA")
     exit = labels.pop("ZZ")
     assert len(entrance) == 1
@@ -52,11 +52,10 @@ def solve_maze(grid):
         pos: set(pos2 for pos2 in neighbours(pos) if pos2 in passages)
         for pos in passages
     }
-    for positions in labels.values():
-        assert len(positions) == 2
-        pos1, pos2 = positions
-        graph[pos1].add(pos2)
-        graph[pos2].add(pos1)
+    for label, positions in labels.items():
+        assert len(positions) >= 2
+        for pos1, pos2 in itertools.permutations(positions, 2):
+            graph[pos1].add(pos2)
     # Dijkstra
     distances = dict()
     queue = collections.deque([(0, entrance)])
@@ -142,7 +141,7 @@ def run_tests():
 
 def get_solutions():
     grid = get_grid_from_file()
-    # print(solve_maze(grid))
+    print(solve_maze(grid))
 
 
 if __name__ == "__main__":
