b4724cf273e750be5e91aa735a51fa8ab31c8263
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-10 23:32:07 +0100

    Day 24 - part 2

--- day24.py
+++ day24.py
@@ -5,25 +5,42 @@ import collections
 size = 5
 
 
-def get_bugs_from_lines(lines):
+def make_point(i, j):
+    return (i, j)
+
+
+def make_point3d(i, j):
+    return (i, j, 0)
+
+
+def get_bugs_from_lines(lines, make_point_func=make_point):
     for i, line in enumerate(lines):
         for j, val in enumerate(line):
             if val == "#":
-                yield i, j
+                yield make_point_func(i, j)
 
 
-def get_bugs_from_file(file_path="day24_input.txt"):
+def get_bugs_from_file(file_path="day24_input.txt", make_point_func=make_point):
     with open(file_path) as f:
-        return set(get_bugs_from_lines(f))
+        return set(get_bugs_from_lines(f, make_point_func))
 
 
 def show_bugs(bugs):
-    i_vals = [i for i, j in bugs]
-    j_vals = [j for i, j in bugs]
-    i_range = list(range(min(i_vals), max(i_vals) + 1))
-    j_range = list(range(min(j_vals), max(j_vals) + 1))
+    i_range = list(range(size))
+    j_range = list(range(size))
     for i in i_range:
         print("".join("#" if (i, j) in bugs else "." for j in j_range))
+
+
+def show_bugs3d(bugs):
+    k_vals = [k for _, _, k in bugs]
+    k_range = list(range(min(k_vals), max(k_vals) + 1))
+    i_range = list(range(size))
+    j_range = list(range(size))
+    for k in k_range:
+        print("Depth", k, ":")
+        for i in i_range:
+            print("".join("#" if (i, j, k) in bugs else "." for j in j_range))
 
 
 def neighbours(pos):
@@ -31,11 +48,33 @@ def neighbours(pos):
     for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         i2, j2 = i + di, j + dj
         if 0 <= i2 < size and 0 <= j2 < size:
-            yield i + di, j + dj
+            yield i2, j2
 
 
-def next_gen(bugs):
-    neigh_count = collections.Counter(n for b in bugs for n in neighbours(b))
+def neighbours3d(pos):
+    i, j, k = pos
+    for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        i2, j2 = i + di, j + dj
+        if (i2, j2) == (2, 2):  # Neighbour with the middle
+            assert 0 in (di, dj) and (di, dj) != (0, 0)
+            if di == 0:
+                for i2 in range(size):
+                    yield i2, j - dj, k + 1
+            if dj == 0:
+                for j2 in range(size):
+                    yield i - di, j2, k + 1
+        elif not (0 <= i2 < size):  # Neighbour with border
+            assert 0 <= j2 < size
+            yield i - di, 2, k - 1
+        elif not (0 <= j2 < size):  # Neighbour with border
+            assert 0 <= i2 < size
+            yield 2, j - dj, k - 1
+        else:
+            yield i2, j2, k
+
+
+def next_gen(bugs, neigh_func):
+    neigh_count = collections.Counter(n for b in bugs for n in neigh_func(b))
     next_bugs = set()
     for b, count in neigh_count.items():
         if count == 1 and b in bugs:
@@ -45,16 +84,29 @@ def next_gen(bugs):
     return next_bugs
 
 
-def first_repeated_layout(bugs):
+def first_repeated_layout(bugs, neigh_func=neighbours):
     seen = set()
     bugs = tuple(bugs)
     while bugs not in seen:
         seen.add(bugs)
-        bugs = tuple(next_gen(bugs))
+        bugs = tuple(next_gen(bugs, neigh_func))
     return sum(2 ** (i * size + j) for i, j in bugs)
 
 
-def run_tests():
+def get_bugs_after_n_generations(bugs, n, neigh_func=neighbours):
+    for i in range(n):
+        bugs = next_gen(bugs, neigh_func)
+    return bugs
+
+
+def run_tests_part1():
+    assert sorted(neighbours((2, 3))) == [
+        (1, 3),
+        (2, 2),
+        (2, 4),
+        (3, 3),
+    ]
+    assert sorted(neighbours((0, 0))) == [(0, 1), (1, 0)]
     bugs = [
         "....#",
         "#..#.",
@@ -64,11 +116,74 @@ def run_tests():
     ]
     bugs = set(get_bugs_from_lines(bugs))
     assert first_repeated_layout(bugs) == 2129920
+    assert len(get_bugs_after_n_generations(bugs, 2)) == 12
+    assert len(get_bugs_after_n_generations(bugs, 3)) == 13
+    assert len(get_bugs_after_n_generations(bugs, 4)) == 10
+
+
+def run_tests_part2():
+    # Tile 19
+    assert sorted(neighbours3d((3, 3, 0))) == [
+        (2, 3, 0),
+        (3, 2, 0),
+        (3, 4, 0),
+        (4, 3, 0),
+    ]
+    # Tile G
+    assert sorted(neighbours3d((1, 1, 0))) == [
+        (0, 1, 0),
+        (1, 0, 0),
+        (1, 2, 0),
+        (2, 1, 0),
+    ]
+    # Tile D
+    assert sorted(neighbours3d((0, 3, 0))) == [
+        (0, 2, 0),
+        (0, 4, 0),
+        (1, 2, -1),
+        (1, 3, 0),
+    ]
+    # Tile E
+    assert sorted(neighbours3d((0, 4, 0))) == [
+        (0, 3, 0),
+        (1, 2, -1),
+        (1, 4, 0),
+        (2, 3, -1),
+    ]
+    # Tile 14/N
+    assert sorted(neighbours3d((2, 3, 0))) == [
+        (0, 4, 1),
+        (1, 3, 0),
+        (1, 4, 1),
+        (2, 4, 0),
+        (2, 4, 1),
+        (3, 3, 0),
+        (3, 4, 1),
+        (4, 4, 1),
+    ]
+    bugs = [
+        "....#",
+        "#..#.",
+        "#..##",
+        "..#..",
+        "#....",
+    ]
+    bugs = set(get_bugs_from_lines(bugs, make_point3d))
+    assert len(get_bugs_after_n_generations(bugs, 10, neighbours3d)) == 99
+
+
+def run_tests():
+    run_tests_part1()
+    run_tests_part2()
 
 
 def get_solutions():
+    # Part 1
     bugs = get_bugs_from_file()
     print(first_repeated_layout(bugs))
+    # Part 2
+    bugs = get_bugs_from_file(make_point_func=make_point3d)
+    print(len(get_bugs_after_n_generations(bugs, 200, neighbours3d)))
 
 
 if __name__ == "__main__":
