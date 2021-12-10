8c2a54268696fccb6a66d6603ad2260d3de2839e
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-10 18:22:10 +0100

    Day 22 - part 2 - WIP

--- day22.py
+++ day22.py
@@ -8,6 +8,7 @@ def get_orders_from_file(file_path="day22_input.txt"):
         return [l.strip() for l in f]
 
 
+# Basic operations
 def new_deck(nb):
     return list(range(nb))
 
@@ -30,6 +31,24 @@ def deal_with_increment(deck, n):
     return deck2
 
 
+# Reverse operation
+def reverse_deal_into_new_stack(nb_cards, final_position):
+    return nb_cards - final_position - 1
+
+
+def reverse_cut_n_cards(nb_cards, n, final_position):
+    return (final_position + n) % nb_cards
+
+
+def reverse_deal_with_increment(nb_cards, n, final_position):
+    assert math.gcd(nb_cards, n) == 1
+    # Card i ends up at position j such that: j = (n*i) % nb_cards
+    # Finding i from j corresponds to finding the inverse of n modulo nb_cards
+    # To be continued
+    pass
+
+
+# Orders
 def apply_order(order, deck):
     if order == "deal into new stack":
         return deal_into_new_stack(deck)
@@ -48,7 +67,7 @@ def apply_orders(nb, orders):
     return deck
 
 
-def run_tests():
+def test_minimal_operations():
     deck = new_deck(10)
     assert deck == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert deal_into_new_stack(deck) == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
@@ -56,6 +75,24 @@ def run_tests():
     assert cut_n_cards(deck, -4) == [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
     assert deal_with_increment(deck, 3) == [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
     assert deck == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def test_reverse_operations():
+    for nb_cards in [10, 20]:
+        deck = deal_into_new_stack(new_deck(nb_cards))
+        for pos, card in enumerate(deck):
+            assert reverse_deal_into_new_stack(nb_cards, pos) == card
+        for cut_arg in [3, -4]:
+            deck = cut_n_cards(new_deck(nb_cards), cut_arg)
+            for pos, card in enumerate(deck):
+                assert reverse_cut_n_cards(nb_cards, cut_arg, pos) == card
+        for incr_arg in [3]:
+            deck = deal_with_increment(new_deck(nb_cards), incr_arg)
+            for pos, card in enumerate(deck):
+                print(reverse_deal_with_increment(nb_cards, incr_arg, pos), card)
+
+
+def test_orders():
     orders = [
         "deal with increment 7",
         "deal into new stack",
@@ -89,9 +126,29 @@ def run_tests():
     assert apply_orders(10, orders) == [9, 2, 5, 8, 1, 4, 7, 0, 3, 6]
 
 
+def run_tests():
+    test_minimal_operations()
+    test_reverse_operations()
+    test_orders()
+
+
+def part1(orders):
+    nb_cards = 10007
+    card_number = 2019
+    return apply_orders(nb_cards, orders).index(card_number)
+
+
+def part2(orders):
+    nb_cards = 119315717514047
+    final_position = 2020
+    # Perform operations in reverse
+    # Hope to see same positions twice and infer a frequence
+
+
 def get_solutions():
     orders = get_orders_from_file()
-    print(apply_orders(10007, orders).index(2019))
+    print(part1(orders))
+    print(part2(orders))
 
 
 if __name__ == "__main__":
