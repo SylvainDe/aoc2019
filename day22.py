19a601c3db500bee62be975e1cea9bc0f8299687
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-11 11:44:42 +0100

    Day 22 - part 1 - optiomisation

--- day22.py
+++ day22.py
@@ -9,7 +9,7 @@ def get_orders_from_file(file_path="day22_input.txt"):
         return [l.strip() for l in f]
 
 
-# Basic operations
+# Basic operations on full deck
 def new_deck(nb):
     return list(range(nb))
 
@@ -32,21 +32,33 @@ def deal_with_increment(deck, n):
     return deck2
 
 
-# Reverse operation
-def reverse_deal_into_new_stack(nb_cards, final_position):
-    return nb_cards - final_position - 1
+# Basic operations on single card
+def single_deal_into_new_stack(nb_cards, position):
+    return nb_cards - position - 1
 
 
-def reverse_cut_n_cards(nb_cards, n, final_position):
-    return (final_position + n) % nb_cards
+def single_cut_n_cards(nb_cards, n, position):
+    return (position - n) % nb_cards
 
 
-def reverse_deal_with_increment(nb_cards, n, final_position):
+def single_deal_with_increment(nb_cards, n, position):
+    return (n * position) % nb_cards
+
+
+# Reverse operation on single card
+reverse_deal_into_new_stack = single_deal_into_new_stack
+
+
+def reverse_cut_n_cards(nb_cards, n, position):
+    return single_cut_n_cards(nb_cards, -n, position)
+
+
+def reverse_deal_with_increment(nb_cards, n, position):
     assert math.gcd(nb_cards, n) == 1
     # Card i ends up at position j such that: j = (n*i) % nb_cards
     # Finding i from j corresponds to finding the inverse of n modulo nb_cards
     n_inv = modinv(n, nb_cards)
-    return final_position * n_inv % nb_cards
+    return position * n_inv % nb_cards
 
 
 def xgcd(a, b):
@@ -91,7 +103,28 @@ def apply_orders(nb, orders):
     return deck
 
 
-# Reverse orders
+# Orders on single cards
+def apply_single_order(order, nb_cards, position):
+    if order == "deal into new stack":
+        return single_deal_into_new_stack(nb_cards, position)
+    orders = [
+        ("cut ", single_cut_n_cards),
+        ("deal with increment ", single_deal_with_increment),
+    ]
+    for prefix, func in orders:
+        if order.startswith(prefix):
+            param = int(order[len(prefix) :])
+            return func(nb_cards, param, position)
+    assert False
+
+
+def apply_single_orders(orders, nb_cards, position):
+    for order in orders:
+        position = apply_single_order(order, nb_cards, position)
+    return position
+
+
+# Reverse orders on single cards
 def apply_reverse_order(order, nb_cards, position):
     if order == "deal into new stack":
         return reverse_deal_into_new_stack(nb_cards, position)
@@ -122,24 +155,28 @@ def test_minimal_operations():
     assert deck == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-def test_reverse_operations():
+def test_single_card_operations():
     for nb_cards in [10, 20]:
         deck = deal_into_new_stack(new_deck(nb_cards))
         for pos, card in enumerate(deck):
+            assert single_deal_into_new_stack(nb_cards, card) == pos
             assert reverse_deal_into_new_stack(nb_cards, pos) == card
         for cut_arg in [3, -4]:
             deck = cut_n_cards(new_deck(nb_cards), cut_arg)
             for pos, card in enumerate(deck):
+                assert single_cut_n_cards(nb_cards, cut_arg, card) == pos
                 assert reverse_cut_n_cards(nb_cards, cut_arg, pos) == card
         for incr_arg in [3]:
             deck = deal_with_increment(new_deck(nb_cards), incr_arg)
             for pos, card in enumerate(deck):
+                assert single_deal_with_increment(nb_cards, incr_arg, card) == pos
                 assert reverse_deal_with_increment(nb_cards, incr_arg, pos) == card
 
 
-def test_reverse_orders(nb_cards, orders):
+def test_single_card_orders(nb_cards, orders):
     result = apply_orders(nb_cards, orders)
     for pos, card in enumerate(result):
+        assert apply_single_orders(orders, nb_cards, card) == pos
         assert apply_reverse_orders(orders, nb_cards, pos) == card
 
 
@@ -151,21 +188,21 @@ def test_orders():
         "deal into new stack",
     ]
     assert apply_orders(nb_cards, orders) == [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]
-    test_reverse_orders(nb_cards, orders)
+    test_single_card_orders(nb_cards, orders)
     orders = [
         "cut 6",
         "deal with increment 7",
         "deal into new stack",
     ]
     assert apply_orders(nb_cards, orders) == [3, 0, 7, 4, 1, 8, 5, 2, 9, 6]
-    test_reverse_orders(nb_cards, orders)
+    test_single_card_orders(nb_cards, orders)
     orders = [
         "deal with increment 7",
         "deal with increment 9",
         "cut -2",
     ]
     assert apply_orders(nb_cards, orders) == [6, 3, 0, 7, 4, 1, 8, 5, 2, 9]
-    test_reverse_orders(nb_cards, orders)
+    test_single_card_orders(nb_cards, orders)
     orders = [
         "deal into new stack",
         "cut -2",
@@ -179,19 +216,19 @@ def test_orders():
         "cut -1",
     ]
     assert apply_orders(nb_cards, orders) == [9, 2, 5, 8, 1, 4, 7, 0, 3, 6]
-    test_reverse_orders(nb_cards, orders)
+    test_single_card_orders(nb_cards, orders)
 
 
 def run_tests():
     test_minimal_operations()
-    test_reverse_operations()
+    test_single_card_operations()
     test_orders()
 
 
 def part1(orders):
     nb_cards = 10007
     card_number = 2019
-    return apply_orders(nb_cards, orders).index(card_number)
+    return apply_single_orders(orders, nb_cards, card_number)
 
 
 def part2(orders):
