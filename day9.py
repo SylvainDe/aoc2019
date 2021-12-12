b603f1cc2cc0a5d83a91191cd17449522ee5ecc6
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-12 17:21:19 +0100

    Day 9 - part 2

--- day9.py
+++ day9.py
@@ -10,6 +10,7 @@ def run_tests():
 def get_solutions():
     intcode = int_code.get_intcode_from_file("day9_input.txt")
     print(int_code.run(intcode, input_=1)[1])
+    print(int_code.run(intcode, input_=2)[1])
 
 
 if __name__ == "__main__":
